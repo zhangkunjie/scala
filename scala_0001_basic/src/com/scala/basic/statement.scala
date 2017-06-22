7de680f0cb41588d31c8cd29dec27a380c48a525
kunjie <kunjie.zhang@corp.elong.com>
2017-06-22 14:51:08 +0800

    AAAAAAAA-version1.0

---
+++ scala_0001_basic/src/com/scala/basic/statement.scala
@@ -0,0 +1,21 @@
+package com.scala.basic
+import scala.math._
+/**
+ * @author Administrator
+ */
+object iftest{
+  def main(args: Array[String]): Unit = {
+    val s="hello word!"
+    println(s)
+    var x=0
+    x-1
+    println(x)
+    val greeting:String="Hello"
+    println(greeting)
+    println(99.99.toInt)
+    //print(x++)scala没有++操作
+    println(sqrt(9))
+    println("hello"(1))
+    
+  }
+}
